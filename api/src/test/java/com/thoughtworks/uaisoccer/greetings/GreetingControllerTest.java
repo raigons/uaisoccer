11108127dca997ef528bdf4fe353e68ae9aa7802
Adriano Lisboa Corrêa <adrianolic@gmail.com>
2015-04-27 17:22:41 -0300

    Configurating spring tests and h2 db
    @adrianolisboa / @raqlem / @raigons

--- api/src/test/java/com/thoughtworks/uaisoccer/greetings/GreetingControllerTest.java
+++ api/src/test/java/com/thoughtworks/uaisoccer/greetings/GreetingControllerTest.java
@@ -1,22 +1,46 @@
 package com.thoughtworks.uaisoccer.greetings;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestContext;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import javax.annotation.Resource;
+
+import static org.hamcrest.core.Is.is;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(classes = {TestContext.class, WebApplicationContext.class })
-@WebAppConfiguration()
+@ContextConfiguration(locations = {"file:src/test/resources/test-application-context.xml"})
+@WebAppConfiguration
 public class GreetingControllerTest {
 
-    // http://www.javacodegeeks.com/2013/08/unit-testing-of-spring-mvc-controllers-rest-api.html
+    private MockMvc mockMvc;
+
+    @Resource
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setUp() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    }
 
     @Test
-    public void requestGreeting() throws Exception {
-
+    public void shouldReturnTheExpectedJson() throws Exception {
+        mockMvc.perform(get("/greeting"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.id", is(1)))
+            .andExpect(jsonPath("$.content", is("Hello, World!")));
     }
 }
