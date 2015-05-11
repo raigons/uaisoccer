ae9d306ea161525b2c6de93f2d0e12695663b756
Felipe Dornelas <fdornela@thoughtworks.com>
2015-05-11 16:39:06 -0300

    Fixing unused imports

--- api/src/test/java/com/thoughtworks/uaisoccer/greetings/GreetingControllerTest.java
+++ api/src/test/java/com/thoughtworks/uaisoccer/greetings/GreetingControllerTest.java
@@ -1,16 +1,12 @@
 package com.thoughtworks.uaisoccer.greetings;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
