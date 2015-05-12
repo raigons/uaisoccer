930c51da35a3ee64ff0a48a1483aa41964763114
michelbueno <bueno.michel@gmail.com>
2015-05-12 15:18:15 -0300

    Refactoring championship store tests. @michelbueno / @felipead

--- api/src/test/java/com/thoughtworks/uaisoccer/championships/ChampionshipStoreTest.java
+++ api/src/test/java/com/thoughtworks/uaisoccer/championships/ChampionshipStoreTest.java
@@ -4,12 +4,15 @@ import com.thoughtworks.uaisoccer.BaseIntegrationTest;
 import org.hamcrest.MatcherAssert;
 import org.hamcrest.Matchers;
 import org.hibernate.Query;
-import org.junit.*;
+import org.junit.Before;
+import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.util.List;
 
 import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.hamcrest.core.IsNull.notNullValue;
 import static org.junit.Assert.assertThat;
 
 public class ChampionshipStoreTest extends BaseIntegrationTest{
@@ -29,17 +32,28 @@ public class ChampionshipStoreTest extends BaseIntegrationTest{
     }
 
     @Test
-    public void shouldSaveANewChampionship() {
-        Championship brazilianLeague = new Championship();
-        brazilianLeague.setName("Brasileirao");
+    public void shouldCreateChampionship() {
+        Championship championship = new Championship();
+        championship.setName("Brasileirao");
 
-        Long championshipId = store.create(brazilianLeague);
+        Long id = store.create(championship);
 
-        assertThat(championshipId, is(brazilianLeague.getId()));
+        assertThat(id, is(notNullValue()));
+        assertThat(championship.getId(), is(id));
+
+        Query query = getSession().createQuery("from Championship where id = :id");
+        query.setParameter("id", id);
+
+        @SuppressWarnings("unchecked")
+        List<Championship> queryResult = (List<Championship>)query.list();
+        assertThat(queryResult.contains(championship), is(true));
+
+        Championship createdChampionship = queryResult.get(queryResult.indexOf(championship));
+        assertThat(createdChampionship, is(equalTo(championship)));
     }
 
     @Test
-    public void shouldUpdateAnExistingChampionship() {
+    public void shouldUpdateExistingChampionship() {
         championship.setName("UEFA Champions League");
         store.update(championship);
 
