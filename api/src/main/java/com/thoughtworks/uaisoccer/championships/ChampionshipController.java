5e2396645534cc32ccf117a65307cc9f9d8dda2a
michelbueno <bueno.michel@gmail.com>
2015-05-15 11:58:11 -0300

    Fixing @Transaction annotation problem

--- api/src/main/java/com/thoughtworks/uaisoccer/championships/ChampionshipController.java
+++ api/src/main/java/com/thoughtworks/uaisoccer/championships/ChampionshipController.java
@@ -5,10 +5,12 @@ import com.thoughtworks.uaisoccer.common.ObjectNotFoundException;
 import com.thoughtworks.uaisoccer.common.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping(value = "/championships")
+@Transactional
 public class ChampionshipController extends BaseController<Championship> {
 
     @Autowired
--- api/src/main/java/com/thoughtworks/uaisoccer/common/Store.java
+++ api/src/main/java/com/thoughtworks/uaisoccer/common/Store.java
@@ -3,7 +3,6 @@ package com.thoughtworks.uaisoccer.common;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-@Transactional
 @Service
 public @interface Store {
 }
