0c316c3daef6eaa85858fb37b46f0fe6183900fd
michelbueno <bueno.michel@gmail.com>
2015-05-21 15:03:31 -0300

    Replacing annotation @TestExecutionListeners for @DirtiesContext

--- api/src/test/java/com/thoughtworks/uaisoccer/BaseIntegrationTest.java
+++ api/src/test/java/com/thoughtworks/uaisoccer/BaseIntegrationTest.java
@@ -4,22 +4,16 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.TestExecutionListeners;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
-import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
-import org.springframework.test.context.transaction.TransactionalTestExecutionListener;
 import org.springframework.transaction.annotation.Transactional;
 
 
 @Transactional
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {"file:src/test/resources/test-application-context.xml"})
-@TestExecutionListeners({
-        DependencyInjectionTestExecutionListener.class,
-        DirtiesContextTestExecutionListener.class,
-        TransactionalTestExecutionListener.class})
+@DirtiesContext
 public abstract class BaseIntegrationTest {
 
     @Autowired
