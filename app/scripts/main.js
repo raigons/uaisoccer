60e142a7fcc83bce512ba0e7707f1d0647ff0136
Adriano Lisboa Corrêa <adrianolic@gmail.com>
2015-05-26 19:13:51 -0300

    Creating team app

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,2 +1,30 @@
-/* jshint devel:true */
-console.log('\'Allo \'Allo!');
+function TeamViewModel() {
+  function Team(name) {
+    this.name = ko.observable(name);
+  }
+
+  this.teams = ko.observableArray([
+    new Team("Cruzeiro"),
+    new Team("Atletico MG"),
+    new Team("São Paulo")
+  ]);
+
+  this.save = function(){
+    $.ajax({
+       url: "http://localhost:8080/api/teams/",
+       type: "POST",
+       crossDomain: true,
+       data: ko.toJSON(name),
+       dataType: "json",
+       success: function (response) {
+           var resp = JSON.parse(response)
+           alert(resp.status);
+       },
+       error: function (xhr, status) {
+           alert("error");
+       }
+     });
+  };
+}
+
+ko.applyBindings(new TeamViewModel());
