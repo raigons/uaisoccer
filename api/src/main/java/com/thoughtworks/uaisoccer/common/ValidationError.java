3bbec6353ffc25a0cff9d04a91e5c6486518ac20
Felipe Dornelas <felipead@gmail.com>
2015-06-03 14:35:59 -0300

    Removing null fields from error response.
    @felipead / @raqlem

--- api/src/main/java/com/thoughtworks/uaisoccer/common/ValidationError.java
+++ api/src/main/java/com/thoughtworks/uaisoccer/common/ValidationError.java
@@ -1,9 +1,11 @@
 package com.thoughtworks.uaisoccer.common;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ValidationError {
 
     private String message;
-
     private String field;
 
     public ValidationError(String field, String message) {
